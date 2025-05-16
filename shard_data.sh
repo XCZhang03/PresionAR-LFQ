3115cd14b7984b14c905cde0dd4ee5078c9e2a6c
Haowei Lin <linhw@a234.cluster.helixon.com>
2025-05-16 20:56:41 +0800

    add logging dir

--- shard_data.sh
+++ shard_data.sh
@@ -3,9 +3,9 @@
 #SBATCH -t 0-08:00          # Runtime in D-HH:MM, minimum of 10 minutes
 #SBATCH -p serial_requeue   # Partition to submit to
 #SBATCH --mem=100           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 
 cd /n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit
 conda activate maskbit
-python scripts/create_sharded_dataset.py --data="./data" --shards="./shards"
+python scripts/create_sharded_dataset.py --data="./data" --shards="./shards" 
