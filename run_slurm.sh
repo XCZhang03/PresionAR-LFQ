408f976b7ada11d205a3e7701a2154bfcf0fe601
Haowei Lin <linhw@a234.cluster.helixon.com>
2025-05-19 10:12:12 +0800

    test slurm

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#SBATCH --job-name=multinode
-#SBATCH -D .
-#SBATCH --output=O-%x.%j
-#SBATCH --error=E-%x.%j
-#SBATCH --nodes=4                   # number of nodes
+#SBATCH --job-name=vae-1
+#SBATCH -p gpu
+#SBATCH --mem=10G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=504985967@qq.com
+#SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodes=2                   # number of nodes
 #SBATCH --ntasks-per-node=1         # number of MP tasks
-#SBATCH --gres=gpu:4                # number of GPUs per node
+#SBATCH --gres=gpu:nvidia_a100-pcie-40gb:4                # number of GPUs per node
 #SBATCH --cpus-per-task=160         # number of cores per tasks
-#SBATCH --time=01:59:00             # maximum execution time (HH:MM:SS)
+#SBATCH -t 0-01:00                  # maximum execution time (HH:MM:SS)
 
 ######################
 ### Set enviroment ###
@@ -23,21 +26,22 @@ export GPUS_PER_NODE=4
 head_node_ip=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 ######################
 
-export LAUNCHER="accelerate launch \
+LAUNCHER="accelerate launch \
     --num_processes $((SLURM_NNODES * GPUS_PER_NODE)) \
     --num_machines $SLURM_NNODES \
     --rdzv_backend c10d \
     --main_process_ip $head_node_ip \
     --main_process_port 29500 \
     "
-export ACCELERATE_DIR="$/home/linhw/zxc/PresionAR-LFQ/maskbit"
-export SCRIPT="${ACCELERATE_DIR}/sscripts/train_res_tokenizer.py"
+ACCELERATE_DIR="/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit"
+cd $ACCELERATE_DIR
+SCRIPT="${ACCELERATE_DIR}/sscripts/train_res_tokenizer.py"
 
 ## change the batch size according to GPU memory
 SCRIPT_ARGS="
     config=${ACCELERATE_DIR}/configs/tokenizer/rqbit_tokenizer_10bit.yaml \
-    training.per_gpu_batch_size=32 \ 
-    training.max_train_steps=300_000 \
+    training.per_gpu_batch_size=8 \ 
+    training.max_train_steps=1_350_000 \
     training.mixed_precision='no' \
     dataset.params.train_shards_path_or_url=./shards/train/imagenet-train-{0000..0252}.tar  \
     dataset.params.val_shards_path_or_url=./shards/val/imagenet-val-{0000..0009}.tar \
