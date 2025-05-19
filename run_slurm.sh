3bdc7a762a7578d4d7eacfd75e6b826bff2c31d0
Haowei Lin <linhw@a234.cluster.helixon.com>
2025-05-19 15:56:42 +0800

    add eval

--- run_slurm.sh
+++ run_slurm.sh
@@ -58,4 +58,4 @@ SCRIPT_ARGS="
     
 # This step is necessary because accelerate launch does not handle multiline arguments properly
 CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
-srun $CMD
+eval "srun $CMD"
