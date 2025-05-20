523561671b9eb8f374a119177fca730cec2aac99
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-05-20 09:21:21 +0800

    test on gpu

--- run_slurm.sh
+++ run_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=vae-1
-#SBATCH -p gpu_test
+#SBATCH -p gpu
 #SBATCH --mem=50G
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=504985967@qq.com
@@ -9,15 +9,15 @@
 #SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
 #SBATCH --nodes=2                   # number of nodes
 #SBATCH --ntasks-per-node=1         # number of MP tasks
-#SBATCH --gres=gpu:1                # number of GPUs per node
-#SBATCH -t 0-01:00                  # maximum execution time (HH:MM:SS)
+#SBATCH --gres=gpu:2                # number of GPUs per node
+#SBATCH -t 0-02:00                  # maximum execution time (HH:MM:SS)
 #SBATCH --contiguous
 
 ######################
 ### Set enviroment ###
 ######################
 source activateEnvironment.sh
-GPUS_PER_NODE=1
+GPUS_PER_NODE=2
 export LOG_LEVEL=INFO
 ######################
 
@@ -54,7 +54,7 @@ srun bash -c "accelerate launch \
     --machine_rank $SLURM_PROCID \
     $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
     config=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit.yaml \
-    training.per_gpu_batch_size=8"
+    training.per_gpu_batch_size=32"
 
 
 
