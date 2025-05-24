59562d5f1494b7499a6206f65eb9f9af4631543f
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-05-24 10:58:20 +0800

    change port

--- debug_slurm.sh
+++ debug_slurm.sh
@@ -40,7 +40,7 @@ srun bash -c "
     --num_processes $NUM_PROCESSES \
     --num_machines $NNODES \
     --main_process_ip $head_node_ip \
-    --main_process_port 7100 \
+    --main_process_port 0 \
     --machine_rank $SLURM_PROCID \
     $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
     config=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit_2lvl.yaml \
