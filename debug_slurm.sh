d6c1e3a534ceb532c6cb7572a94065bee69810e8
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-05-24 10:54:16 +0800

    debug multilevel

--- debug_slurm.sh
+++ debug_slurm.sh
@@ -43,7 +43,7 @@ srun bash -c "
     --main_process_port 29500 \
     --machine_rank $SLURM_PROCID \
     $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
-    config=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit.yaml \
+    config=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit_2lvl.yaml \
     training.per_gpu_batch_size=8 \
     training.gradient_accumulation_steps=1 \
     experiment.save_every=100 \
