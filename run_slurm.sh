46f02c4e84db8e06f63c17e816bb60e0202bdd39
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-11 08:59:27 +0800

    prepare from scratch training

--- run_slurm.sh
+++ run_slurm.sh
@@ -39,7 +39,8 @@ NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
 ####################
 ### Set run name ###
 ####################
-RUN_NAME="2level-mixed_after_1lvl-long"
+# RUN_NAME="2level-mixed_after_1lvl-long"
+RUN_NAME="2level-mixed_from_scratch-long"
 ####################
 
 srun bash -c "
@@ -58,7 +59,7 @@ srun bash -c "
     experiment.save_every=2_000 \
     experiment.resume=true \
     experiment.run_name=${RUN_NAME} \
-    experiment.init_checkpoint=$ACCELERATE_DIR/runs/outputs/rqbit_tokenizer_10bit/2level/2025-05-25-04-13-13-EDT/checkpoints/checkpoint_3 \
+    experiment.init_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit/runs/outputs/rqbit_tokenizer_10bit/2level/current/checkpoints/checkpoint_105 \
     "
 
 
