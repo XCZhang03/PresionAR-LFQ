a70bd446f5c61ed6c23cacdd1a0e4f4824e2a8e4
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-15 22:43:47 +0800

    fix config file

--- run_slurm.sh
+++ run_slurm.sh
@@ -62,7 +62,7 @@ srun bash -c "
     experiment.save_every=1_000 \
     experiment.resume=true \
     experiment.run_name=${RUN_NAME} \
-    experiment.init_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit/runs/outputs/rqbit_tokenizer_10bit/2level-mixed_from_scratch-long/checkpoints/checkpoint_213 \
+    experiment.init_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit/runs/outputs/rqbit_tokenizer_10bit/2level-mixed_from_scratch-long/checkpoints/checkpoint_253 \
     "
 
 
