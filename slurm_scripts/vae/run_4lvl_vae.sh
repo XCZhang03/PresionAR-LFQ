b0b18889b4984eaeb8c2bc447c446b4392a5bf5f
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-16 22:04:30 +0800

    prepare 4 lvl vae job

---
+++ slurm_scripts/vae/run_4lvl_vae.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+#SBATCH --job-name=vae-4-from_scratch
+#SBATCH -p kempner_requeue
+#SBATCH --mem=100G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=504985967@qq.com
+#SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodes=2                   # number of nodes
+#SBATCH --ntasks-per-node=1         # number of MP tasks
+#SBATCH --cpus-per-task=16           # number of CPU cores per task
+#SBATCH --gres=gpu:nvidia_a100-sxm4-40gb:4                # number of GPUs per node
+#SBATCH -t 4-00:00                  # maximum execution time (HH:MM:SS)
+#SBATCH --contiguous
+#SBATCH --account=kempner_sham_lab
+
+######################
+### Set enviroment ###
+######################
+source activateEnvironment.sh
+GPUS_PER_NODE=4
+export LOG_LEVEL=INFO
+######################
+
+######################
+#### Set network #####
+######################
+head_node_ip=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+######################
+
+######################
+## Set launcher ######
+######################
+NNODES=$SLURM_NNODES
+NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
+######################
+
+####################
+### Set run name ###
+####################
+RUN_NAME="4level-from_scratch-long"
+####################
+
+
+###################
+### Config file ###
+###################
+config_file=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit_4lvl.yaml
+###################
+
+srun bash -c "
+    accelerate launch \
+    --multi_gpu \
+    --rdzv_backend c10d \
+    --num_processes $NUM_PROCESSES \
+    --num_machines $NNODES \
+    --main_process_ip $head_node_ip \
+    --main_process_port 29500 \
+    --machine_rank $SLURM_PROCID \
+    $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
+    config=$config_file \
+    training.per_gpu_batch_size=16 \
+    training.gradient_accumulation_steps=2 \
+    experiment.save_every=1_000 \
+    experiment.resume=true \
+    experiment.run_name=${RUN_NAME} \
+    "
+
+
+
