bc4d40611decef35cfc96593a5eb909e8b0d2801
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-19 09:36:01 +0800

    change partition

--- slurm_scripts/vae/run_4lvl_vae.sh
+++ slurm_scripts/vae/run_4lvl_vae.sh
@@ -7,10 +7,10 @@
 #SBATCH --mail-user=504985967@qq.com
 #SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
-#SBATCH --nodes=2                   # number of nodes
+#SBATCH --nodes=1                   # number of nodes
 #SBATCH --ntasks-per-node=1         # number of MP tasks
 #SBATCH --cpus-per-task=16           # number of CPU cores per task
-#SBATCH --gres=gpu:nvidia_a100-sxm4-40gb:4                # number of GPUs per node
+#SBATCH --gres=gpu:nvidia_h100_80gb_hbm3:4               # number of GPUs per node
 #SBATCH -t 6-00:00                  # maximum execution time (HH:MM:SS)
 #SBATCH --contiguous
 #SBATCH --account=kempner_sham_lab
@@ -61,7 +61,7 @@ srun bash -c "
     --machine_rank $SLURM_PROCID \
     $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
     config=$config_file \
-    training.per_gpu_batch_size=16 \
+    training.per_gpu_batch_size=32 \
     training.gradient_accumulation_steps=2 \
     experiment.save_every=1_000 \
     experiment.resume=true \
