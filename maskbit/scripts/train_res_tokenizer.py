2b69d2d021665bee5dcaa8de5b03a127daa4c578
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-19 17:06:16 +0800

    add archive for every 100k steps

--- maskbit/scripts/train_res_tokenizer.py
+++ maskbit/scripts/train_res_tokenizer.py
@@ -702,7 +702,14 @@ def save_checkpoint(model, output_dir, accelerator, global_step) -> Path:
         json.dump({"global_step": global_step}, (save_path / "metadata.json").open("w+"))
         logger.info(f"Saved state to {save_path}")
 
+        if global_step % 100_000 == 0:
+            os.makedirs(f"{output_dir}/archive", exist_ok=True)
+            import shutil
+            shutil.copytree(save_path, f"{output_dir}/archive/checkpoint-{global_step}", dirs_exist_ok=True)
+            print(f"Checkpoint {global_step} archived.")
+
     
+
     return save_path
 
 
