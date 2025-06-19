9785d5fb1d4d7fd690528dac0185e664b42f7077
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-19 09:30:46 +0800

    change ckpt path

--- slurm_scripts/vae/run_4lvl_vae.sh
+++ slurm_scripts/vae/run_4lvl_vae.sh
@@ -66,7 +66,7 @@ srun bash -c "
     experiment.save_every=1_000 \
     experiment.resume=true \
     experiment.run_name=${RUN_NAME} \
-    experiment.init_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit/runs/outputs/rqbit_tokenizer_10bit/1level/current/checkpoints/checkpoint_111  \
+    experiment.init_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-LFQ/maskbit/runs/outputs/rqbit_tokenizer_10bit/2level/2025-05-25-04-13-13-EDT/checkpoints/checkpoint_3  \
     "
 
 
