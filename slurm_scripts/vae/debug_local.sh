e9d86369edff09cddd61c7142aec0feff59dce7e
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-23 22:01:51 +0800

    refactor script dirs

---
+++ slurm_scripts/vae/debug_local.sh
@@ -0,0 +1,50 @@
+# !/bin/bash
+
+source activateEnvironment.sh
+
+export ACCELERATE_DIR="/datapool/data2/home/linhw/zhangxiangcheng/DiffAR/PrecisionAR-LFQ/maskbit"
+cd $ACCELERATE_DIR
+######################
+### Set GPUs #########
+######################
+GPUS_PER_NODE=1
+export CUDA_VISIBLE_DEVICES=6,7
+######################
+
+
+LAUNCHER="accelerate launch \
+    --num_processes $((1 * GPUS_PER_NODE)) \
+    --num_machines 1 \
+    "
+
+SCRIPT="${ACCELERATE_DIR}/scripts/train_res_tokenizer.py"
+
+####################
+### Set run name ###
+####################
+RUN_NAME="4lvl-test-sd-2"
+####################
+
+
+## change the batch size according to GPU memory
+SCRIPT_ARGS="
+    config=${ACCELERATE_DIR}/configs/tokenizer/rqbit_tokenizer_10bit_4lvl.yaml \
+    training.per_gpu_batch_size=16 \
+    training.gradient_accumulation_steps=2 \
+    dataset.params.train_shards_path_or_url=./shards/train/imagenet-train-{0000..0008}.tar \
+    dataset.params.eval_shards_path_or_url=./shards/val/imagenet-val-0000.tar \
+    experiment.save_every=100 \
+    experiment.generate_every=100 \
+    experiment.eval_every=100 \
+    experiment.run_name=${RUN_NAME} \
+    experiment.logger=tensorboard \
+    model.vq_model.finetune_decoder=true \
+    model.vq_model.schedule_type="weighted"  \
+    model.vq_model.weights=[1,0,0,0] \
+    experiment.init_checkpoint="${ACCELERATE_DIR}/runs/outputs/rqbit_tokenizer_10bit/4lvl-test-loss_weight/archive/checkpoint-100" \
+    "
+    
+# This step is necessary because accelerate launch does not handle multiline arguments properly
+CMD="$LAUNCHER $SCRIPT $SCRIPT_ARGS"
+echo "Running command: $CMD"
+$CMD
---
+++ slurm_scripts/vae/debug_slurm.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+#SBATCH --job-name=vae-debug
+#SBATCH -p gpu_test
+#SBATCH --mem=100G
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=504985967@qq.com
+#SBATCH -o status/myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --nodes=2                   # number of nodes
+#SBATCH --ntasks-per-node=1         # number of MP tasks
+#SBATCH --cpus-per-task=8           # number of CPU cores per task
+#SBATCH --gres=gpu:1                # number of GPUs per node
+#SBATCH -t 0-01:00                  # maximum execution time (HH:MM:SS)
+#SBATCH --contiguous
+
+######################
+### Set enviroment ###
+######################
+source activateEnvironment.sh
+GPUS_PER_NODE=1
+export LOG_LEVEL=INFO
+######################
+
+######################
+#### Set network #####
+######################
+head_node_ip=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+######################
+
+######################
+## Set launcher ######
+######################
+NNODES=$SLURM_NNODES
+NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
+######################
+
+####################
+### Set run name ###
+####################
+RUN_NAME="2level-large_batch"
+####################
+
+srun bash -c "
+    accelerate launch \
+    --multi_gpu \
+    --rdzv_backend c10d \
+    --num_processes $NUM_PROCESSES \
+    --num_machines $NNODES \
+    --main_process_ip $head_node_ip \
+    --main_process_port 29500 \
+    --machine_rank $SLURM_PROCID \
+    $ACCELERATE_DIR/scripts/train_res_tokenizer.py \
+    config=$ACCELERATE_DIR/configs/tokenizer/rqbit_tokenizer_10bit_2lvl.yaml \
+    training.per_gpu_batch_size=8 \
+    training.gradient_accumulation_steps=1 \
+    experiment.save_every=100 \
+    experiment.generate_every=100 \
+    experiment.eval_every=400 \
+    experiment.resume=true \
+    experiment.run_name=${RUN_NAME} \
+    "
+
+
