ea5e842f5290b43e16f0142a16fbf5a81b87faa1
Haowei Lin <linhw@a226.cluster.helixon.com>
2025-06-29 08:45:44 +0800

    test model args

--- slurm_scripts/vae/run_4lvl_vae.sh
+++ slurm_scripts/vae/run_4lvl_vae.sh
@@ -88,7 +88,7 @@ srun bash -c "
     experiment.save_every=1_000 \
     experiment.resume=true \
     experiment.run_name=${RUN_NAME} \
-    ${MODEL_ARGS} \
+    model.vq_model.scales=[1,0.8,0.6,0.4] \
     "
 
 
