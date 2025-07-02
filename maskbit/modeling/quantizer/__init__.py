a54e29f9bbe4935a5d0df9c54b6da6e7b4d503b6
Haowei Lin <linhw@a234.cluster.helixon.com>
2025-07-02 16:37:47 +0800

    resume concat concat job

--- maskbit/modeling/quantizer/__init__.py
+++ maskbit/modeling/quantizer/__init__.py
@@ -1,2 +1,4 @@
 from .quantizer import SimpleVectorizer
 from .lookup_free import LookupFreeQuantizer
+from .residual_vq import ResidualVQ
+from .residual_lfq import ResidualLFQ
--- slurm_scripts/cond_gen/run_2lvl_cond.sh
+++ slurm_scripts/cond_gen/run_2lvl_cond.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=embed-adaln-2lvl
+#SBATCH --job-name=concat-concat-2lvl
 #SBATCH -p kempner_requeue
 #SBATCH --mem=100G
 #SBATCH --mail-type=FAIL
@@ -39,10 +39,10 @@ NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
 ####################
 ### Set run name ###
 ####################
-# RUN_NAME="2lvl-concat-concat"
+RUN_NAME="2lvl-concat-concat"
 # RUN_NAME="2lvl-embed-concat"
 # RUN_NAME="2lvl-adaln-adaln"
-RUN_NAME="2lvl-embed-adaln"
+# RUN_NAME="2lvl-embed-adaln"
 ####################
 
 ###################
@@ -65,16 +65,16 @@ vqgan_checkpoint=/n/holylfs06/LABS/sham_lab/Users/ydu/zhangxiangcheng/PresionAR-
 # MODEL_ARGS="model.mlm_model.context_conditioning=embed \
 #     model.mlm_model.label_conditioning=concat \
 #     "
-# MODEL_ARGS="model.mlm_model.context_conditioning=concat \
-#     model.mlm_model.label_conditioning=concat \
-#     model.mlm_model.tie_context_pos_embeddings=true \
-#     "
+MODEL_ARGS="model.mlm_model.context_conditioning=concat \
+    model.mlm_model.label_conditioning=concat \
+    model.mlm_model.tie_context_pos_embeddings=true \
+    "
 # MODEL_ARGS="model.mlm_model.context_conditioning=adaln \
 #     model.mlm_model.label_conditioning=adaln \
 #     "
-MODEL_ARGS="model.mlm_model.context_conditioning=embed \
-    model.mlm_model.label_conditioning=adaln \
-    "
+# MODEL_ARGS="model.mlm_model.context_conditioning=embed \
+#     model.mlm_model.label_conditioning=adaln \
+#     "
 ###################
 
 srun bash -c "
