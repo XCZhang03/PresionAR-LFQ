4dad8f6a6e8aae170fe46a80f48f46806b7c3e7b
Xiangcheng Zhang <xczhang@boslogin06.rc.fas.harvard.edu>
2025-08-03 03:53:45 -0400

    eval base ckpt

--- slurm_scripts/maskbit/eval_maskbit.sh
+++ slurm_scripts/maskbit/eval_maskbit.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-#SBATCH --job-name=eval-ar
-#SBATCH -p kempner_requeue
+#SBATCH --job-name=eval-maskbit
+#SBATCH -p kempner_h100
 #SBATCH --mem=100G
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=504985967@qq.com
 #SBATCH -o status/myeval_%j.out  # File to which STDOUT will be written, %j inserts jobid
 #SBATCH -e status/myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
+#SBATCH --open-mode=append
 #SBATCH --nodes=1                  # number of nodes
 #SBATCH --ntasks-per-node=1         # number of MP tasks
 #SBATCH --cpus-per-task=16           # number of CPU cores per task
 #SBATCH --gres=gpu:nvidia_h100_80gb_hbm3:1                # number of GPUs per node
-#SBATCH -t 0-10:00                  # maximum execution time (HH:MM:SS)
+#SBATCH -t 0-12:00                  # maximum execution time (HH:MM:SS)
 #SBATCH --contiguous
 #SBATCH --account=kempner_ydu_lab
 
@@ -31,13 +32,13 @@ date_str=$(date +%Y-%m-%d_%H-%M-%S)
 #######################
 #### checkpoint #######
 #######################
-vqgan_checkpoint=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/ckpts/maskbit_tokenizer_12bit.bin
-ar_checkpoint=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/ckpts/maskbit_generator_12bit.bin
+vqgan_checkpoint=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/ckpts/maskbit_tokenizer_10bit.bin
+ar_checkpoint=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/ckpts/maskbit_generator_10bit.bin
 
 ###################
 ### Config file ###
 ###################
-config_file=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/maskbit/configs/generator/maskbit_generator_12bit.yaml
+config_file=/n/holylabs/ydu_lab/Lab/zhangxiangcheng/code/PresionAR-LFQ/maskbit/configs/generator/maskbit_generator_10bit.yaml
 ###################
 
 ####################
